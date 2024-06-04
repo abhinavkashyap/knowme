1fd2c936287c95ed67e41261817a22e59537e89b
abhinavkashyap <abhinav@comp.nus.edu.sg>
2024-06-04 13:11:39 +0800

    Feat: Persist the vector store and load if available
    Creating the vectorstore is a call to the LLM. It has cost associated
    with it. WE can now do this only once and persist the store on disk
    We can then load the store from disk and answer questions. This is
    a simple mechanism to persist the store.

--- knowme/embedder.py
+++ knowme/embedder.py
@@ -4,10 +4,16 @@ from langchain.text_splitter import TextSplitter
 from langchain_chroma import Chroma
 from langchain_core.embeddings import Embeddings
 from langchain_core.vectorstores import VectorStore
+from pathlib import Path
 
 
 class ChromaEmbedder:
-    def __init__(self, splitter: TextSplitter, embedding_function: Embeddings):
+    def __init__(
+        self,
+        splitter: TextSplitter,
+        embedding_function: Embeddings,
+        embedding_store_directory: Optional[str],
+    ):
         """This stores the embedding of the documents into a vector store
         This only considers the RecursiveCharacterTextSplitter for now
         Other TextSplitters might be implemented later
@@ -18,15 +24,22 @@ class ChromaEmbedder:
             A splitter that splits the documents into smaller pieces of text
         embedding_function: Embeddings
             An Embedding function
+        embeddings_store_directory: Optional[str]
+            If this is provided, then the `store_embeddings` will
+            store the emebeddings in this store. Further calls to the
+            `store_embeddings` will retrieve the store from this
+            directory
         """
         self.splitter = splitter
         self.embedding_function = embedding_function
+        self.embedding_store_directory = embedding_store_directory
 
     def store_embeddings(
-        self, texts: list[str], metadatas: Optional[list[dict]]
+        self, texts: Optional[list[str]] = None, metadatas: Optional[list[dict]] = None
     ) -> VectorStore:
         """This creates the vector store from the embeddings and then returns it
-
+        Either the mebeddings are created based on the texts and metadatas that
+        you pass or loaded from the directory
         Parameters
         ----------
         texts : list[str]
@@ -39,11 +52,23 @@ class ChromaEmbedder:
         VectorStore
             Vector store that helps in retrieval
         """
-        documents = self.splitter.create_documents(texts=texts, metadatas=metadatas)
-        split_documents = self.splitter.split_documents(documents)
-        vectorstore = Chroma.from_documents(
-            documents=split_documents,
-            embedding=self.embedding_function,
-        )
+
+        if (
+            self.embedding_store_directory
+            and not Path(self.embedding_store_directory).isdir()
+        ):
+            assert texts is not None, f"Pass the texts to be embedded into the store"
+            documents = self.splitter.create_documents(texts=texts, metadatas=metadatas)
+            split_documents = self.splitter.split_documents(documents)
+            vectorstore = Chroma.from_documents(
+                documents=split_documents,
+                embedding=self.embedding_function,
+                persist_directory=self.embedding_store_directory,
+            )
+        else:
+            return Chroma(
+                persist_directory=str(self.embedding_store_directory),
+                embedding_function=self.embedding_function,
+            )
 
         return vectorstore
